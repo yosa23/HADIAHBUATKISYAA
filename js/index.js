315176820b1150cae4d7fa1c79b75ebcf92c5388
yosa23 <99300138+yosa23@users.noreply.github.com>
2024-08-21 20:18:51 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Segg, Ini buat kamuuu'.split('')
+const text = 'Selamatt Ulang Tahun Sayangggg, Inii buat kamuuuu'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
